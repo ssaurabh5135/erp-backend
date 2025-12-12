2ca0ffd299eb317f26488d588c77b809a72e57dc
ssaurabh5135 <ssaurabh5135@gmail.com>
2025-12-12 12:34:11 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,1 +1,19 @@
+# app/database.py
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:password@localhost:5432/erpdb"
+)
+
+# create engine
+engine = create_engine(DATABASE_URL, echo=False)
+
+# Session factory
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base class for models
+Base = declarative_base()
 
